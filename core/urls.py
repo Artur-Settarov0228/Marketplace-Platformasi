bab5d128ee19291825bee7865de15493ee95896d
Artur-Settarov0228 <settarovartur2@gmail.com>
2026-03-10 21:49:07 +0500

    init

--- core/urls.py
+++ core/urls.py
@@ -14,24 +14,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.conf import settings
+from django.conf.urls.static import static
 
+
+from drf_spectacular.views import SpectacularAPIView,SpectacularSwaggerView,SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("swagger/", SpectacularSwaggerView.as_view(url_name="schema")),
-
-    path("api/v1/", include("apps.users.urls")),
-    path("api/v1/", include("apps.sellers.urls")),
-    path("api/v1/", include("apps.products.urls")),
-    path("api/v1/", include("apps.categories.urls")),
-    path("api/v1/", include("apps.favorites.urls")),
-    path("api/v1/", include("apps.orders.urls")),
-    path("api/v1/", include("apps.reviews.urls")),
+    path('api/v1/', include('apps.users.urls')),
+    path('api/v1/', include('apps.sellers.urls')),
+    path('api/v1/', include('apps.categories.urls')),
+    path('api/v1/', include('apps.products.urls')),
+    path('api/v1/', include('apps.favorites.urls')),
+    path('api/v1/', include('apps.orders.urls')),
+    path('api/v1/', include('apps.reviews.urls')),
+    
+    path('api/v1/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/v1/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
